56314a247ce7d0c4d9d9201e876b02d704f684f6
Karthik Nukala <karthik.nukala@sri.com>
2026-04-07 12:57:47 -0700

    gitignores + sal-version

--- src/sal-version.scm
+++
@@ -1,9 +0,0 @@
-;; Automatically generated file (don't edit)
-(module sal-version
-        (export (salenv/build-date)
-                (salenv/version))
-        (eval (export-exports)))
-
-(define (salenv/build-date) "Wed Mar  4 07:50:49 PST 2026")
-
-(define (salenv/version) "3.3")
